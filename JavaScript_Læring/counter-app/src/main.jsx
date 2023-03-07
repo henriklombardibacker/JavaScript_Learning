f251d7e3715017a96b62ad3d91c91574302e07f0
henriklombardibacker <117280176+henriklombardibacker@users.noreply.github.com>
2023-03-07 14:58:09 +0100

    Failed React Vite Experiment

--- JavaScript_Læring/counter-app/src/main.jsx
+++ JavaScript_Læring/counter-app/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './Components/Counter';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
 )

aaed73f03f82df87277da25eee8bf347a662b911
henriklombardibacker <117280176+henriklombardibacker@users.noreply.github.com>
2023-03-10 13:45:31 +0100

    Ny React-Props tutorial og ny mappestruktur

---
+++ JavaScript_Læring/React-Vite-tutorials/props-tutorial/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Joke from './Joke'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Dad Jokes</h1>
+      <Joke />
+      <Joke />
+    </div>
+  )
+}
+
+export default App

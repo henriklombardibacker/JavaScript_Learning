130a2cecb8b5e636ebe981a49bbb0dc929223738
henriklombardibacker <117280176+henriklombardibacker@users.noreply.github.com>
2023-03-14 08:56:51 +0100

    Ble ferdig med denne tutorialen.
    https://www.youtube.com/watch?v=f640Z6QZawc&list=PL0X6fGhFFNTe_vJIlAQQo0IEgPgk9er3g&index=4&ab_channel=SamMeech-Ward

--- JavaScript_Læring/React-Vite-tutorials/props-tutorial/src/App.jsx
+++ JavaScript_Læring/React-Vite-tutorials/props-tutorial/src/App.jsx
@@ -5,26 +5,31 @@ function App() {
 
   const jokes = [
     {
+    id: 1,
     joke: "I used to be a banker, but then i lost interest.",
     rating: 4
     }, {
+      id: 2,
       joke: "Im afraid for the calender. Its days are numbered.",
       rating: 4
     }, {
+      id: 3,
       joke: "What did the ocean say to the beach? Nothing, it just waved.",
       rating: 3
     }, {
+      id: 4,
       joke: "I only know 25 letters of the alphabet. I don't know y.",
       rating: 5
     }
  ]
 
-  return (
-    <div className="App">
+ 
+ return (
+   <div className="App">
       <h1>Dad Jokes</h1>
-      {jokes.map((joke, index) => {
-        return <Joke key={index} Joke={joke.joke} rating={joke.rating}/>
-      })}
+      {jokes.map((joke => (
+       <Joke key={joke.id} Joke={joke.joke} rating={joke.rating} />)
+      ))}  
     </div>
   )
 }

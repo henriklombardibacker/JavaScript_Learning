413083432ee16c08c2d4b143cb7a1a9f6f7bede1
henriklombardibacker <117280176+henriklombardibacker@users.noreply.github.com>
2023-03-24 15:10:09 +0100

    useState array of objects tutorial initial commit

--- JavaScript_Læring/React-Vite-tutorials/usestate-tutorial/src/App.jsx
+++ JavaScript_Læring/React-Vite-tutorials/usestate-tutorial/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   const [favorite, setFavorite] = useState(1)
 
-  const jokes =[
+  const [jokes, setJokes] = useState([
     {
       id: 1,
       text: "I'm afraid for the calendar. Its days are numbered."
@@ -16,13 +16,18 @@ function App() {
       id: 2,
       text: "I'm afraid to be addicted to soap, but I'm clean now."
     }
-  ]
+  ])
  
   const handleFavorite = (id) => {
     setFavorite(id)
   }
 
   const handleNewJoke = (text) => {
+    const joke = {
+      text, 
+      id: self.crypto.randomUUID()
+    }
+    setJokes([joke, ...jokes])
     console.log("", text)
   }
 
@@ -30,11 +35,11 @@ function App() {
     <div className="App">
     <h1>Dad Jokes</h1>
 
+    <JokeForm onNewJoke={handleNewJoke}/>
+
     {jokes.map(joke => (
       <Joke onFavorite={handleFavorite} favorite={favorite === joke.id} key={joke.id} id={joke.id} text={joke.text} />
     ))}
-
-    <JokeForm onNewJoke={handleNewJoke}/>
 
     </div>
   )
